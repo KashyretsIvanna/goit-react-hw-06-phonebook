a59a190e97f077fb03d0c1baaf7a4a8d95c501ad
ZadAndRot <kasirecivanna@gmail.com>
2022-08-03 16:04:13 +0300

    feat: :bug: jj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,7 @@ const App = ({ contactsFromRedux, setContacts }) => {
 			console.log(items);
 			setContacts(items);
 		}
-	}, []);
+	}, [setContacts]);
 
 	return (
 		<div className={styles.app}>
