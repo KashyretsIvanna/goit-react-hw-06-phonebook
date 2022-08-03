61090f97c1b4e916c0b4f8092ade2b781426e1ce
ZadAndRot <kasirecivanna@gmail.com>
2022-08-03 16:03:30 +0300

    feat: :bug: deleted unimportant imports

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -1,7 +1,7 @@
 import contactsReducer from './phonebook-reducer';
 import { configureStore } from '@reduxjs/toolkit';
 // import { getDefaultMiddleware } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 // const middleware = [...getDefaultMiddleware(), logger];
 const store = configureStore({
