d317ab8e125b40a5fc23ef7292578a313d5eff3b
ZadAndRot <kasirecivanna@gmail.com>
2022-08-03 16:01:07 +0300

    feat: :bug: deleted middleware

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -1,14 +1,14 @@
 import contactsReducer from './phonebook-reducer';
 import { configureStore } from '@reduxjs/toolkit';
-import { getDefaultMiddleware } from '@reduxjs/toolkit';
+// import { getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 
-const middleware = [...getDefaultMiddleware(), logger];
+// const middleware = [...getDefaultMiddleware(), logger];
 const store = configureStore({
 	reducer: {
 		contacts: contactsReducer,
 	},
-	middleware,
+	// middleware,
 });
 
 export default store;
